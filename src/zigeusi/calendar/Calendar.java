5c0098f9654106cdbf797d26cce9eb984920b626
zigeusi <geagulgeagul@gmail.com>
2019-07-27 18:36:29 +0900

    Add repeat function

--- src/zigeusi/calendar/Calendar.java
+++ src/zigeusi/calendar/Calendar.java
@@ -15,12 +15,19 @@ public class Calendar {
 
 		Scanner scanner = new Scanner(System.in);
 		Calendar cal = new Calendar();
+		System.out.println("반복 횟수를 입력하세요.");
+		int repeat = scanner.nextInt();
+		
+		
+		for(int i = 0; i < repeat; i++) {
 		System.out.println("달을 입력하세요 : ");
 		int month = scanner.nextInt();
-
 		System.out.printf("%d월은 %d일까지 있습니다.\n", month, cal.getMaxDaysOfMonth(month));
+		
+		}
+		System.out.println("Bye~");
 		scanner.close();
-
 	}
+	
 
 }
